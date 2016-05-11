1d9f62c07bc2eef6305ac4ea531a885625a229a2
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2016-05-12 00:56:08 +0200

    bugfix

--- build.py
+++ build.py
@@ -30,4 +30,5 @@ with open("README.md", "w") as f:
 
 	#f.write("Original source: [Paper sizes chart](http://resources.printhandbook.com/pages/paper-size-chart.php)\n")
 
-	f.write(r'# License\n<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.')
+	f.write("# License\n")
+	f.write(r'<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.')
